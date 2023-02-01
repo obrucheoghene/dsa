8608e674b2a57964d9c5d0af9989f35c02382f4e
Wilfred <wilfredcloudspace@gmail.com>
2023-02-01 22:02:53 +0100

    Update ans in JS to us push array method to populate array

--- 412_fizzbuzz/ans.js
+++ 412_fizzbuzz/ans.js
@@ -7,17 +7,20 @@ var fizzBuzz = function(n) {
     let i = 1;
     while(i <= n){
         if (i % 15 === 0){
-            answer[i - 1] = "FizzBuzz"; 
+            // answer[i - 1] = "FizzBuzz"; 
+            answer.push("FizzBuzz"); 
         }
         else if (i % 5 === 0){
-            answer[i - 1] = "Buzz"; 
+            // answer[i - 1] = "Buzz";
+            answer.push("Buzz");  
         }
         else if (i % 3 === 0){
-            answer[i - 1] = "Fizz"; 
+            // answer[i - 1] = "Fizz";
+            answer.push("Fizz");  
         }
         else{
-            answer[i - 1] = i.toString(); 
-
+            // answer[i - 1] = i.toString(); 
+            answer.push(i.toString()); 
         }
         i++;
     }
