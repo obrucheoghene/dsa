cc7921401582d4e3015cc4cf9342f325ba356ef9
Wilfred <wilfredcloudspace@gmail.com>
2023-02-09 01:21:12 +0100

    fix: Fix bug in answer by adding sum variable

--- expore_leet/max_consecutive_ones/ans.java
+++ expore_leet/max_consecutive_ones/ans.java
@@ -1,14 +1,18 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int max = 0;
-            
+        int sum = 0;    
             for(int num : nums) {
                 if (num == 1){
-                    max++;
+                    sum++;
                 }else {
-                    max = 0;
+                    if (sum > max)
+                        max = sum;
+                    sum = 0;
                 }
             }
+        if (sum > max)
+                max = sum;
         return max;
     }
 }
